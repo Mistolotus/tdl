23355c093c2661a7652df18ba3d8cf04db8012e2
Mistsink <1589760052@qq.com>
2023-03-31 12:52:58 +0800

    feat: 🎸 extract the internal funcs interface

---
+++ app/internal_core/tgc.go
@@ -0,0 +1,10 @@
+package internalcore
+
+import "github.com/Mistolotus/tdl/app/internal/tgc"
+
+var (
+	RunWithAuth = tgc.RunWithAuth
+	Login       = tgc.Login
+	New         = tgc.New
+	NoLogin     = tgc.NoLogin
+)
